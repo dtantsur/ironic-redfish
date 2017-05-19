cdd54aded0c42393ddef710211ad8d4bab14b435
Dmitry Tantsur <divius.inside@gmail.com>
2017-05-19 17:24:58 +0200

    More verbose build

--- tools/pip_install.sh
+++ tools/pip_install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+set -eux
+
 # NOTE: using stable/newton to stay compatible with it
 RELEASE=${RELEASE:-newton}
 CONSTRAINTS=https://git.openstack.org/cgit/openstack/requirements/plain/upper-constraints.txt?h=stable/$RELEASE
 
-pip uninstall ironic -y || true
+pip uninstall ironic -y || echo Ironic not found, skipping
 pip install -U -c $CONSTRAINTS $@
 pip install -U -c $CONSTRAINTS -r https://git.openstack.org/cgit/openstack/ironic/tree/requirements.txt?h=stable/$RELEASE
 pip install git+git://git.openstack.org/openstack/ironic@stable/$RELEASE#egg=ironic
